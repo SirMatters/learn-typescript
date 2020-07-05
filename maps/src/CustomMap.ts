45587b7356ec8174852ebb0e63d691644a3232d9
SirMatters <perov.krll@gmail.com>
2020-07-05 21:05:21 +0300

    addMarker functionality - create Mappable interface to describe things which could be mapped to the gmap, all items should comply to the interface - implement addMarker() method with the mappable item as a parameter

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,12 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 /**
  * The class is used to wrap google map to prevent
  * standard methods exposure. Oterwise other devs
@@ -7,12 +16,22 @@
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
